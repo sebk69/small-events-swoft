23c0c0f401eac6c551b268434c6f46413f872299
Sébastien Kus <k.sebastien@la-becanerie.com>
2021-10-31 22:46:32 +0100

    Init repository

---
+++ README.md
@@ -0,0 +1,16 @@
+# sebk/small-events-swoft
+
+Swoft cross applications events dispatcher and receiver
+
+## Install
+
+Create your Swoft project : http://swoft.io/docs/2.x/en/quick-start/install.html
+
+Require Swoft Voter package (https://github.com/sebk69/small-events-swoft) :
+```
+composer require sebk/small-events-swoft
+```
+
+## Documentation
+
+TODO
---
+++ src/AutoLoader.php
@@ -0,0 +1,52 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is a part of sebk/small-events-swoft
+ * Copyright 2021 - Sébastien Kus
+ * Under GNU GPL V3 licence
+ */
+
+namespace Sebk\SmallEventsSwoft;
+
+use Swoft\Helper\ComposerJSON;
+use Swoft\SwoftComponent;
+
+/**
+ * Class AutoLoader
+ *
+ * @since 2.0
+ */
+class AutoLoader extends SwoftComponent
+{
+    /**
+     * @return array
+     */
+    public function beans(): array
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getPrefixDirs(): array
+    {
+        return [
+            __NAMESPACE__ => __DIR__,
+        ];
+    }
+
+    /**
+     * Metadata information for the component.
+     *
+     * @return array
+     * @see ComponentInterface::getMetadata()
+     */
+    public function metadata(): array
+    {
+        $jsonFile = dirname(__DIR__) . '/composer.json';
+
+        return ComposerJSON::open($jsonFile)->getMetadata();
+    }
+}
